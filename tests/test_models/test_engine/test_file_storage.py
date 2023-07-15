2af81c0222f483b4dda15de48ecf6045e7fa2be9
ZinoChan <devzinochan@gmail.com>
2023-07-15 11:47:23 +0100

    init file_storage_test

---
+++ tests/test_models/test_engine/test_file_storage.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import unittest
+from models.base_model import BaseModel
+from models.engine.file_storage import FileStorage
+
+class TestFileStorage(unittest.TestCase):
+    """Unittest for filestorage"""
+    def setUp(self):
+        self.storage = FileStorage()
+        self.storage.reload()
+
+    def test_all(self):
+        o = BaseModel()
+        self.storage.new(o)
+        self.assertEqual(self.storage.all(), {o.__class__.__name__ + '.' + o.id: o})
