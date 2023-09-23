6d9ae462db6719def80828577b344b13a14f71cd
ZinoChan <devzinochan@gmail.com>
2023-09-23 11:57:29 +0100

    fix import

--- models/base_model.py
+++ models/base_model.py
@@ -7,13 +7,12 @@ class BaseModel:
     """
     Defines all common attributes/methods for other classes.
     """
-
     def __init__(self, *args, **kwargs):
-        if kwargs:
+        if len(kwargs) != 0:
             for key, value in kwargs.items():
                 if key != "__class__":
                     if key in ["created_at", "updated_at"]:
-                        value = datetime.datetime.strptime(
+                        value = datetime.strptime(
                             value, "%Y-%m-%dT%H:%M:%S.%f"
                         )
                     setattr(self, key, value)
@@ -38,3 +37,8 @@ class BaseModel:
         dict_rep["created_at"] = dict_rep["created_at"].isoformat()
         dict_rep["updated_at"] = dict_rep["updated_at"].isoformat()
         return dict_rep
+
+
+bm1 = BaseModel()
+bm2 = BaseModel(**bm1.to_dict())
+print(bm1.id == bm2.id)
